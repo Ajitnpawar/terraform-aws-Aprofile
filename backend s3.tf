1f4a37f8e231d0bd3ea092bf2097d914503f9194
Ajitnpawar <87314679+Ajitnpawar@users.noreply.github.com>
2023-02-25 20:09:36 +0530

    vps security grp has been created.

--- backend s3.tf
+++ backend s3.tf
@@ -1,5 +1,5 @@
-terraform{
- backend "s3" {
+terraform {
+  backend "s3" {
     bucket = "vprofilr-ajit-artifactsbkt"
     key    = "terraform/backend"
     region = "us-east-1" // Replace with your preferred region
